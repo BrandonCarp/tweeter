174128d13f265d3ade1b8746c138e9e78871486a
Brandon <brancarp93@gmail.com>
2022-09-29 18:43:12 -0400

    light & dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "className",
   theme: {
-    extend: {},
+    screens: {
+      xms: "320px",
+      sm: "480px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        primaryDark: "#111114",
+        secondaryDark: "#131316",
+        secondaryOrange: "#262833",
+      },
+      fontSize: {
+        xs: ".75rem",
+        sm: ".875rem",
+        base: "1rem",
+        baselg: "1.5rem",
+        md: "1.75rem",
+        l: "2rem",
+        xl: "2.5rem",
+        xxl: "3rem",
+        xxxl: "3.5rem",
+        xlxl: "4rem",
+      },
+    },
   },
   plugins: [],
 };
