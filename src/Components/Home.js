184684b18a47557c0cc8f93b92103423494c4969
Brandon C <brancarp93@gmail.com>
2022-10-05 21:51:57 -0400

    tiny updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,13 +6,12 @@ export function Home() {
   return (
     <>
 
-    <div className=" relative mx-auto container">
-      <div className="flex flex-col items-center ">
-      <h1 className="">Tweeter</h1>
-      <p></p>
-      <div className="md:w-[80%] lg:w-[70%]">
+    <div className=" relative mx-auto container  w-full">
+      <div className="flex flex-col items-center justify-center">
+      <h1>Connecting people online everyday</h1>
+     
       <img className="" src={homeImg } alt='friends laughing'/>
-      </div>
+     
       </div>
 
       
