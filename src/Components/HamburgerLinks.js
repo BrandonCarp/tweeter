b4604614ff1d867e132a3eb614b7793735990fb1
Brandon C <brancarp93@gmail.com>
2022-10-10 21:29:38 -0400

    restarting a bit

--- src/Components/HamburgerLinks.js
+++ src/Components/HamburgerLinks.js
@@ -1,5 +1,10 @@
+import { useNavigate } from "react-router-dom"
+import {GrClose} from 'react-icons/gr'
+
 export function HamburgerLinks() {
+  const navigate = useNavigate();
   return <>
+  <button className="" onClick={()=> navigate(-1)}><GrClose /></button>
   <h1>Hamburger Menu</h1>
   </>
 }
