beff92b08a1a68f2c2b111a4f8952cabe84825dc
Brandon <brancarp93@gmail.com>
2022-10-11 17:44:36 -0400

    home page updates and creation of about

---
+++ src/Components/About.js
@@ -0,0 +1,15 @@
+export function About() {
+  return (
+    <>
+      <div className="relative container mx-auto">
+        <div>
+          <h1>Tweeter</h1>
+          <p>
+            Tweeter is a application that is designed to help people connect,
+            and find communities of interest.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,42 +1,33 @@
-import { useState } from "react"
-import {  NavLink } from "react-router-dom"
-import {GiHamburgerMenu} from 'react-icons/gi'
-
-
-
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export function Navbar() {
-
   const [burgerMenu, setBurgerMenu] = useState(false);
- 
-  return <>
 
-
-
-
-
- 
-  <nav className="relative mx-auto mb-10 mt-3">
+  return (
+    <>
+      <nav className="relative mx-auto mb-10 mt-3 md:mb-[5rem]">
         <ul className="flex-row items-center   md:pl-10 md:flex ">
           <li className="absolute  left-0 ml-7 mt-3  text-baselg   md:invisible">
             <NavLink to="/hamburgerlink">
-            <GiHamburgerMenu />
+              <GiHamburgerMenu />
             </NavLink>
           </li>
           <li className="text-baselg flex   justify-center  ">
             {/* Make a icon */}
-            <NavLink  to="/">Tweeter</NavLink>
+            <NavLink to="/">Tweeter</NavLink>
           </li>
           <div className="invisible absolute right-0 mr-5  space-x-5 flex md:visible">
-          <li className="">
-            <NavLink to="/usersearch">Search Users</NavLink>
-          </li>
-          <li>
-            <NavLink to="/showcase">Showcase</NavLink>
-          </li>
+            <li className="">
+              <NavLink to="/usersearch">Search Users</NavLink>
+            </li>
+            <li>
+              <NavLink to="/showcase">Showcase</NavLink>
+            </li>
           </div>
-          
         </ul>
       </nav>
-  </>
+    </>
+  );
 }
