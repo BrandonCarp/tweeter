0495eaa768041f012e2ab07e7dc6b0790fe99a8d
Brandon C <brancarp93@gmail.com>
2022-10-11 13:26:03 -0400

    burger menu location moved and few small updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,7 @@
 
 import homeImg from './Imgs/homePage.jpg';
+import { NavLink } from 'react-router-dom';
+import {BsArrowRightCircle} from 'react-icons/bs'
 
 export function Home() {
   
@@ -9,7 +11,10 @@ export function Home() {
     <div className=" relative mx-auto   ">
       <div className="flex flex-col items-center  justify-center space-y-10">
       <h1 className='text-[1.2rem] '>Connecting people online everyday</h1>
-      <img className="" src={homeImg } alt='friends laughing'/>
+      <ul>
+      <li className='hover:animate-bounce'><NavLink to='/usersearch'><BsArrowRightCircle className='inline text-baselg mb-1 mr-2 ' />Find out who's tweeting</NavLink></li>
+      </ul>
+      <img className="p-5 md:p-[4rem] lg:p-[7rem]" src={homeImg } alt='friends laughing'/>
      
       </div>
 
