9e568b85c4e69286a759b3a038482a4188fd1173
Brandon <brancarp93@gmail.com>
2022-10-11 17:44:36 -0400

    home page updates and creation of about

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import {   Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./Components/Navbar";
 import { Home } from "./Components/Home";
 import { TweetShowcase } from "./Components/TweetShowcase";
 import { UserSearch } from "./Components/UserSearch";
-import {HamburgerLinks} from "./Components/HamburgerLinks";
+import { HamburgerLinks } from "./Components/HamburgerLinks";
 
 function App() {
   return (
     <>
       <Navbar />
 
-<Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/usersearch" element={<UserSearch />} />
         <Route path="/showcase" element={<TweetShowcase />} />
         <Route path="/hamburgerlink" element={<HamburgerLinks />} />
       </Routes>
-
-      
     </>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,26 +1,31 @@
-
-import homeImg from './Imgs/homePage.jpg';
-import { NavLink } from 'react-router-dom';
-import {BsArrowRightCircle} from 'react-icons/bs'
+import homeImg from "./Imgs/homePage.jpg";
+import { NavLink } from "react-router-dom";
+import { BsArrowRightCircle } from "react-icons/bs";
+import { About } from "./About";
 
 export function Home() {
-  
   return (
     <>
-
-    <div className=" relative mx-auto   ">
-      <div className="flex flex-col items-center  justify-center space-y-10">
-      <h1 className='text-[1.2rem] '>Connecting people online everyday</h1>
-      <ul>
-      <li className='hover:animate-bounce'><NavLink to='/usersearch'><BsArrowRightCircle className='inline text-baselg mb-1 mr-2 ' />Find out who's tweeting</NavLink></li>
-      </ul>
-      <img className="p-5 md:p-[4rem] lg:p-[7rem]" src={homeImg } alt='friends laughing'/>
-     
+      <div className=" relative mx-auto   ">
+        <div className="flex flex-col items-center  justify-center space-y-5">
+          <h1 className="text-[1.2rem]  md:text-xl">
+            Connecting people online everyday
+          </h1>
+          <ul>
+            <li className="hover:animate-bounce md:text-baselg">
+              <NavLink to="/usersearch">
+                <BsArrowRightCircle className="inline text-baselg mb-1 mr-2 " />
+                Find out who's tweeting
+              </NavLink>
+            </li>
+          </ul>
+          <img
+            className="pt-4 px-5 pb-5 md:px-[4rem] md:pb-[4rem]  lg:w-[1200px] lg:pb-[7rem]"
+            src={homeImg}
+            alt="friends laughing"
+          />
+        </div>
       </div>
-
-      
-    </div>
-
     </>
   );
 }
