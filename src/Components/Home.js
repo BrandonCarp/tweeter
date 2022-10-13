d99a1a2c5e14579d2cc60cea29e55789a6b566bc
Brandon C <brancarp93@gmail.com>
2022-10-13 12:58:24 -0400

    framer motion updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,29 @@ import homeImg from "./Imgs/homePage.jpg";
 import { NavLink } from "react-router-dom";
 import { BsArrowRightCircle } from "react-icons/bs";
 import { About } from "./About";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function Home() {
   return (
     <>
+    <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0, y:-50 }}
+              animate={{ opacity: 1, y:0  }}
+              exit={{ opacity: 0 }}
+              
+            >
       <Navbar />
+      </motion.div>
+      </AnimatePresence>
+
       <div className=" relative mx-auto   ">
         <div className="flex flex-col items-center  justify-center space-y-5">
+          <motion.div  
+          initial={{opacity:0, x:-100}}
+          animate={{opacity:1, x: 0 }} 
+          className="flex flex-col items-center"
+          >
           <h1 className="text-[1.2rem]  md:text-xl">
             Connecting people online everyday
           </h1>
@@ -21,6 +37,7 @@ export function Home() {
               </NavLink>
             </li>
           </ul>
+          </motion.div>
           <img
             className="pt-4 px-5 pb-5 md:px-[4rem] md:pb-[4rem]  lg:w-[1200px] lg:pb-[7rem]"
             src={homeImg}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { AnimatePresence, motion } from "framer-motion";
+
+
 
 export function Navbar() {
   const [burgerMenu, setBurgerMenu] = useState(false);
@@ -15,18 +16,12 @@ export function Navbar() {
               <GiHamburgerMenu />
             </NavLink>
           </li>
-          <AnimatePresence>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, x: 100 }}
-              exit={{ opacity: 0 }}
-            >
+          
               <li className="text-baselg flex   justify-center  ">
                 {/* Make a icon */}
                 <NavLink to="/">Tweeter</NavLink>
               </li>
-            </motion.div>
-          </AnimatePresence>
+        
 
           <div className="invisible absolute right-0 mr-5  space-x-5 flex md:visible">
             <li className="">
