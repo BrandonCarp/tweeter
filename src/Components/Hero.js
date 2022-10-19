b405c11a03afa37084f176cb08f7330613e12ba0
Brandon <brancarp93@gmail.com>
2022-10-19 18:59:24 -0400

    footer updates

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,22 +1,12 @@
-import { Navbar } from "./Navbar";
+import { motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
+import homeImg from "./Imgs/homePage.jpg";
+import { BsArrowRightCircle } from "react-icons/bs";
 
-import { Hero } from "./Hero";
-import { AnimatePresence, motion } from "framer-motion";
-
-export function Home() {
+export function Hero() {
   return (
     <>
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0 }}
-        >
-          <Navbar />
-        </motion.div>
-      </AnimatePresence>
-      <Hero />
-      {/* <div className=" relative mx-auto mt-[5rem]   ">
+      <div className=" relative mx-auto mt-[5rem]   ">
         <div className="flex flex-col items-center  justify-center      space-y-5">
           <motion.div
             initial={{ opacity: 0, x: -100 }}
@@ -38,13 +28,15 @@ export function Home() {
               </li>
             </ul>
           </motion.div>
-          <img
+          <motion.img
+            initial={{ opacity: 0, x: +100 }}
+            animate={{ opacity: 1, x: 0 }}
             className="pt-4 px-5 pb-5 md:px-[4rem] md:pb-[4rem]   lg:w-[1200px]"
             src={homeImg}
             alt="friends laughing"
           />
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
--- src/Components/MobileFooter.js
+++ src/Components/MobileFooter.js
@@ -6,7 +6,16 @@ import { MdPersonSearch } from "react-icons/md";
 export function MobileFooter() {
   return (
     <>
-      <div className="absolute bottom-0 w-screen p-5 mx-auto border-t-[3px]   md:invisible">
+      <div className="invisible   inset-x-0 bottom-0  w-screen h-16 mb-10 p-5 mx-auto   md:visible ">
+        <div className="">
+          <div className="mb-5 border-b-2 border-black-600 mx-[20%]"></div>
+          <div className="flex items-center justify-center ">
+            <h1 className="font-light mb-5">© Tweeter 2022</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-0  w-screen h-[5rem] p-5 mx-auto border-t-[3px]   md:invisible">
         <div className="flex justify-center items-center">
           <ul className="flex space-x-10">
             <li className="hover:bg-gray-300 hover:rounded-full   p-2">
@@ -16,7 +25,7 @@ export function MobileFooter() {
             </li>
             <li className="hover:bg-gray-300 hover:rounded-full  p-2">
               <NavLink to="/usersearch">
-                <MdPersonSearch className="text-md" />
+                <MdPersonSearch className="text-md " />
               </NavLink>
             </li>
             <li className="hover:bg-gray-300 hover:rounded-full  p-2">
@@ -25,14 +34,6 @@ export function MobileFooter() {
               </NavLink>
             </li>
           </ul>
-        </div>
-      </div>
-      <div className="invisible bottom-0 mb-5 md:visible mt-[5rem] lg:mt-0">
-        <div className="">
-          <div className="mb-10 border-b-2 border-black-600 mx-[20%]"></div>
-          <div className="flex items-center justify-center ">
-            <h1 className="font-light">© Tweeter 2022</h1>
-          </div>
         </div>
       </div>
     </>
