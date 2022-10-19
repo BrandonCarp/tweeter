bf283c0582b671e445d0ab74d56bf7761d789932
Brandon <brancarp93@gmail.com>
2022-10-19 19:12:01 -0400

    about section on home page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,10 @@
 export function About() {
   return (
     <>
-      <div className="relative container mx-auto">
-        <div>
-          <h1>Tweeter</h1>
-          <p>
+      <div className="relative container mx-auto mt-5 md:mb-10">
+        <div className="flex  flex-col items-center justify-center">
+          <h1 className="md:text-xl text-md">Tweeter</h1>
+          <p className=" text-sm">
             Tweeter is a application that is designed to help people connect,
             and find communities of interest.
           </p>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import { Navbar } from "./Navbar";
-
+import { About } from "./About";
 import { Hero } from "./Hero";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -16,35 +16,7 @@ export function Home() {
         </motion.div>
       </AnimatePresence>
       <Hero />
-      {/* <div className=" relative mx-auto mt-[5rem]   ">
-        <div className="flex flex-col items-center  justify-center      space-y-5">
-          <motion.div
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            className="flex flex-col items-center "
-          >
-            <h1 className="text-[1.2rem]  md:text-xl">
-              Connecting people online everyday
-            </h1>
-
-            <ul>
-              <li className=" md:text-baselg">
-                <motion.p whileHover={{ scale: 1.1 }}>
-                  <NavLink to="/usersearch">
-                    <BsArrowRightCircle className="inline text-baselg mb-1 mr-2 " />
-                    Find out who's tweeting
-                  </NavLink>
-                </motion.p>
-              </li>
-            </ul>
-          </motion.div>
-          <img
-            className="pt-4 px-5 pb-5 md:px-[4rem] md:pb-[4rem]   lg:w-[1200px]"
-            src={homeImg}
-            alt="friends laughing"
-          />
-        </div>
-      </div> */}
+      <About />
     </>
   );
 }
