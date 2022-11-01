23f2b4358daff5a191fad34ab8ff43d9638d5d29
Brandon C <brancarp93@gmail.com>
2022-11-01 15:55:59 -0400

    search bar updategit add .

--- src/Components/User Search Page/UserSearchBar.js
+++ src/Components/User Search Page/UserSearchBar.js
@@ -1,14 +1,58 @@
+import { useEffect, useState } from "react";
+
 export function UserSearchBar() {
+
+  const [searchInput, setSearchInput] = useState("");
+
+
+const handleSubmit = () => {
+  console.log(`form submitted`)
+}
+
+const onButton = (e) => {
+e.preventDefault();
+
+console.log(searchInput);
+}
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+    
+
+      if (event.key === 'Enter' || event.code === 'NumpadEnter') {
+        event.preventDefault();
+
+        // 👇️ your logic here
+        handleSubmit();
+      }
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
+
+
+  
+
+  
+  
   return (
     <>
-      <div>
+      <form  className="mt-1">
+        <div className=" border-2 border-gray-300">
         <input
           type="search"
-          className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-          placeholder="Search Users"
+          className=" bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none  "
+          placeholder="Search Users" value={searchInput} onChange={event => setSearchInput(event.target.value)}
         />
-        {/* Add button */}
-      </div>
+        <button  className="   bg-gray-400 text-white  h-8 mr-1 px-1">Search</button>
+        </div>
+
+        
+      </form>
     </>
   );
 }
