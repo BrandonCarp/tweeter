2f1699393ce4f11d05d7ef44e55c9ddbd06ad882
Brandon <brancarp93@gmail.com>
2022-10-31 22:40:32 -0400

    small search bar update

--- src/Components/User Search Page/UserSearchBar.js
+++ src/Components/User Search Page/UserSearchBar.js
@@ -7,7 +7,7 @@ export function UserSearchBar() {
           className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
           placeholder="Search Users"
         />
-        {/* Add button, and perhaps add zustand state management */}
+        {/* Add button */}
       </div>
     </>
   );
