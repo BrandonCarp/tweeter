32b64d57d7460fd6e4bc328c6267c8c12fae88dc
Brandon C <brancarp93@gmail.com>
2022-11-03 16:05:22 -0400

    small search bar update

--- src/Components/User Search Page/UserSearchBar.js
+++ src/Components/User Search Page/UserSearchBar.js
@@ -20,11 +20,11 @@ setSearchUser(searchInput);
   return (
     <>
       <form onSubmit={handleSubmit} className="mt-1">
-        <div className="p-2 border-2 border-gray-300 rounded-full flex">
+        <div className="p-2  border-2 border-gray-300 rounded-full flex">
         <button  className="ml-2" type="submit"><BiSearch  className="text-[1.5rem] text-gray-500" /></button>
         <input
           type="search"
-          className=" bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none  "
+          className=" bg-white pl-3 pr-16 rounded-lg text-sm focus:outline-none  "
           placeholder="Search Users" value={searchInput} onChange={event => setSearchInput(event.target.value)}
         />
      
