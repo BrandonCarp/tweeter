34b0842897ec9de127112d2beed8ffc4b3a0cdbf
Brandon <brancarp93@gmail.com>
2022-11-07 18:50:09 -0500

    tweet page update

--- src/Components/User Search Page/Users.js
+++ src/Components/User Search Page/Users.js
@@ -1,8 +1,45 @@
-export function Users() {
-  let people = ["person1", "person2", "person3", "person4", "person5", "person6", "person7", "person8", "person9", "person10"];
-return <div className="flex flex-col justify-center items-center space-y-10">
-{people.map(person => <ul>
-  <li>{person}</li>
-</ul>)}
-</div>
+import kohaku from "./Imgs/me and ko.jpg";
+import { BsChat } from "react-icons/bs";
+import { FaRetweet } from "react-icons/fa";
+import { BiHeart } from "react-icons/bi";
+
+export function Users({ name }) {
+  return (
+    <div className=" relative mx-auto container border w-[85%]  ">
+      <div className="flex container p-8 ">
+        <img
+          src={kohaku}
+          alt="User Photo"
+          className="h-16 mr-5  rounded-full"
+        />
+        <div className="flex flex-col ">
+          <div>
+            <div className="flex">
+              {name}
+              <p className="">@{name}TweeterDevIO</p>
+            </div>
+            <div className="py-4 ">
+              <p>
+                第三章 第七章 第五章 第十章. 復讐者」. 復讐者」. .復讐者」
+                伯母さん . 第七章 第四章 第三章.復讐者」 伯母さん. 第十二章
+                第十一章 第十三章 第十
+              </p>
+            </div>
+          </div>
+
+          <ul className="flex items-center  space-x-5 ">
+            <li className="flex items-center">
+              <BsChat className="mr-2" /> 1337
+            </li>
+            <li className="flex items-center">
+              <FaRetweet className="mr-2" /> 420
+            </li>
+            <li className="flex items-center">
+              <BiHeart className="mr-2" /> 69
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
