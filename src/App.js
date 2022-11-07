47da74a34a7b0dc1d06ade46a085d9e0e3ccf56e
Brandon C <brancarp93@gmail.com>
2022-11-07 13:24:17 -0500

    search bar update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Components/Home Page/Home";
 import { TweetShowcase } from "./Components/TweetShowcase";
+import { Navbar } from "./Components/Home Page/Navbar";
 import { UserSearch } from "./Components/User Search Page/UserSearch";
 import { HamburgerLinks } from "./Components/HamburgerLinks";
 import { MobileFooter } from "./Components/MobileFooter";
-
+import { AnimatePresence, motion } from "framer-motion";
 function App() {
+
   return (
     <div>
-      {/* React Query - Zustand */}
+     
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0 }}
+        >
+          <Navbar />
+        </motion.div>
+      </AnimatePresence>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/usersearch" element={<UserSearch />} />
--- src/Components/Home Page/Home.js
+++ src/Components/Home Page/Home.js
@@ -1,20 +1,12 @@
-import { Navbar } from "./Navbar";
+
 import { Hero } from "./Hero";
 
-import { AnimatePresence, motion } from "framer-motion";
+
 
 export function Home() {
   return (
     <>
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0 }}
-        >
-          <Navbar />
-        </motion.div>
-      </AnimatePresence>
+    
       <Hero />
     </>
   );
