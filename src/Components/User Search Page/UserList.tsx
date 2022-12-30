3d35c6de399079715c52e4f838fc61fb9251bdea
Brandon C <brancarp93@gmail.com>
2022-12-30 13:35:30 -0500

    added an interface for tweets

--- src/Components/User Search Page/UserList.tsx
+++ src/Components/User Search Page/UserList.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {Users} from './Users';
+import kohaku from "./Imgs/me and ko.jpg";
 
 
 
-export const UserList = ({userData} : {userData: string[]}) => {
+export const UserList = ({userData}: {userData: string[]}) => {
   return (
     <>
       {userData.map((userData: string) => (
-        <Users key={userData} name={userData} />
+        <Users key={userData} name={userData} picture={kohaku}/>
       ))}
     </>
   );
--- src/Components/User Search Page/interfaces.ts
+++ src/Components/User Search Page/interfaces.ts
@@ -1,4 +1,10 @@
 
+
 export interface  UserInfo {
   name: string;
+  handle?: string;
+  picture: string;
+  chat?: number;
+  retweet?: number;
+  like?: number;
 }
