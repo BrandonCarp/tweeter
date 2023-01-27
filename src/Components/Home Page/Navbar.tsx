b06d436e89bde37d7477909ce85268528b4e96d6
Brandon <brancarp93@gmail.com>
2023-01-26 21:13:28 -0500

    dark mode styling

--- src/Components/Home Page/Navbar.tsx
+++ src/Components/Home Page/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useContext } from "react";
-import '../Styles/darkMode.css'
+import '../Styles/darkMode.css';
 import ReactSwitch from 'react-switch';
 import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -26,7 +26,7 @@ const {toggleTheme} = useContext(ThemeContext)
 const {theme} = useContext(ThemeContext);
  
   return (
-    <div id={theme} className="text-primaryLight bg-secondaryPurple dark:text-black dark:primaryLight">
+    <div id={theme} className="darkTest ">
       <nav className="sticky top-0 relative container mx-auto p-5 ">
         <ul className="flex-row items-center   md:pl-10 md:flex ">
           <li className="absolute  left-0 ml-7 mt-3  text-baselg   md:invisible">
@@ -36,7 +36,7 @@ const {theme} = useContext(ThemeContext);
 
             <Modal closeModal={closeModal} open={isOpen} 
             >
-              <ul className="space-y-3 text-baselg flex flex-col items-center text-white">
+              <ul className="space-y-3 text-baselg flex flex-col items-center ">
                 <motion.li className="" whileHover={{ scale: 1.1 }}>
                   <NavLink to="/" className=" p-1">
                     <button onClick={closeModal}>Home</button>
@@ -59,7 +59,7 @@ const {theme} = useContext(ThemeContext);
           </li>
 
           <li className="text-baselg flex pt-2   justify-center space-x-5">
-            {/* Make a icon */}
+        
             
             <NavLink to="/" className="font-bold">Tweeter</NavLink>
             
