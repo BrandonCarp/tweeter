184c7341527fea8e6a99ebb4bae29e136750bd2e
Brandon <brancarp93@gmail.com>
2023-02-12 22:24:39 -0500

    update

---
+++ BackEnd/server.js
@@ -0,0 +1,17 @@
+// const http = require("http");
+
+// const HOSTNAME = process.env.HOSTMAN || "localhost";
+// const PORT = process.env.PORT || 3000;
+
+// const server = http.createServer((request, response) => {
+//   response.statusCode = 200;
+//   response.setHeader("Content-Type", "text/plain");
+//   response.end("Hello World");
+// });
+
+// server.listen(PORT, HOSTNAME, () => {
+//   console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+// });
+
+console.log(`File Name${__filename}`);
+console.log(`Directory Name${__dirname}`);
