330e2c4b4a07060e3f8bd3b68e629027ff56e738
Brandon C <brancarp93@gmail.com>
2023-02-14 07:59:59 -0500

    first server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,5 +1,19 @@
-import { addNums } from "./addNums.js";
+const http = require("http");
+const fs = require("fs");
 
-const sum = addNums(2, 2);
+const PORT = process.env.PORT || 3000;
 
-console.log(sum);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  fs.readFile("BackEnd/index.html", (err, data) => {
+    if (err) {
+      console.error(err);
+      response.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
