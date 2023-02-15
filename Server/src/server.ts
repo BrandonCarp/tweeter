64ee2eba169f3786743ebb3b7913d8a305472929
Brandon C <brancarp93@gmail.com>
2023-02-15 13:15:01 -0500

    file structure yay

---
+++ Server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const server = express()
+const PORT = 8000;
+
+server.get('/', (req, res) => {
+return res.send('Hello Sir')
+});
+
+server.listen(PORT, () => {
+  console.log(`Application listening at http://localhost:${PORT}`)
+});
