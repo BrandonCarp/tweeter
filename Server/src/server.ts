87aded6452dd8a2bc2d5f40c3464e40a885cdb46
Brandon C <brancarp93@gmail.com>
2023-03-29 21:22:42 -0400

    api updates

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -23,7 +23,6 @@ app.use(function (req: Request, res: Response, next: NextFunction) {
 
 
 app.get(['/'], (req: Request, res: Response) => {
- 
   res.sendStatus(200)
 })
 
@@ -38,8 +37,31 @@ axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyAT1esraP58Q36M
   console.log(error)
 })
 })
+
+app.get(['/data/channels'], (req: Request, res: Response) => {
+  const search = req.query.q;
+axios.get(`https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAT1esraP58Q36MlSj25D1oOIXgIwo3xq4&part=snippet&q=${search}&type=channel&maxResults=15`)
+.then(function (response: Response) {
+  // @ts-ignore
+  res.send(response.data)
+}).catch(function (error: Error) {
+  console.log(error)
+})
+})
+
+// promise.all()
+// chain axios calls to return a giant object of the channel info, and the video Id's
+
+
+// Search Channels
+// https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyAT1esraP58Q36MlSj25D1oOIXgIwo3xq4&part=snippet&q=coryx&type=channel&maxResults=15
 // routes(app)
 
+// Playlist Search
+// https://www.googleapis.com/youtube/v3/playlists?key=AIzaSyAT1esraP58Q36MlSj25D1oOIXgIwo3xq4&part=snippet&channelId=UCiYcA0gJzg855iSKMrX3oHg&maxResults=20
+
+// Video id Search from playlist
+// https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&playlistId=PLxoHK1S7LhWSIsK-CJIsf7H3Jc2A8mOP9&maxResults=10&key=AIzaSyAT1esraP58Q36MlSj25D1oOIXgIwo3xq4
 
 
 app.listen(PORT, () => {
